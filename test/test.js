c1776b593b8fe21e9b868169b789a4208200fe42
srijan-paul <srijannr1@gmail.com>
2021-02-08 21:08:15 +0530

    added some more snippets and highlight

---
+++ test/test.js
@@ -0,0 +1,20 @@
+let a = 1;
+let b = 2;
+
+for (const k of [1, 2, 3]) {
+  console.log(k)
+}
+
+const mytable = {
+  a: 1,
+  b: 2
+}
+
+let t = mytable.property
+
+function myfn() {
+  return false;
+}
+
+
+let c = myfn()
